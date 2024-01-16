2ca8e25c582e280358cb8ea548768a1a8ac779a1
vishavjeet <lakhanraja364@gmail.com>
2024-01-16 22:52:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useAuth0 } from '@auth0/auth0-react'
+import React, { useState } from 'react';
 function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  console.log("Current User", user)
+
+  
   return (
     <div className="App">
+      <div className="home">
+
+        <nav>
+          <ul>
+            <li><a href="#Home">Home</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Service">Service</a></li>
+            <li><a href="https://lakhanrajaportfolioweb.netlify.app/">Personal</a></li>
+            <li><a href="#Messages">Messages</a></li>
+            <h4> User Authentication System</h4>
+
+          </ul>
+        </nav>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="my-container">
+          <h2> Hi,✋This is MERN Stack Developer</h2>
+          <h3> <marquee> This is a Awesome project for the Affworld Technologies...!</marquee></h3>
+
+        </div>
+        {isAuthenticated && <h1> Welcome To The Affworld Technologies Internship Assignment:
+
+          Full Stack Development (MERN)   {user.name} </h1>
+
+        }
+        {
+          isAuthenticated ? <button onClick={(e) => logout()}> Logout </button> : (
+
+            <button onClick={(e) => loginWithRedirect()}>
+
+              Login Here </button>
+
+          )}
       </header>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+
+    domain="lakhan12.us.auth0.com"
+    clientId="1ODS7oUwY7UQxKTiQucjPaYgSDhYDmTF"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }} >
+      
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
